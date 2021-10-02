edf2c728a29349e54259fbc3487ca05b159ad58b
Christophorus <beneditto1410@gmail.com>
2021-10-02 20:22:20 +0700

    added simple components. Might modify and add later

---
+++ src/components/Expenses.js
@@ -0,0 +1,26 @@
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+function Expenses(props) {
+    return (
+      <Card className="">
+        <ExpenseItem 
+        title={props.expensesDetails[0].title} 
+        amount={props.expensesDetails[0].amount} 
+        date={props.expensesDetails[0].date}/>     
+        <ExpenseItem 
+        title={props.expensesDetails[1].title} 
+        amount={props.expensesDetails[1].amount} 
+        date={props.expensesDetails[1].date}/> 
+        <ExpenseItem 
+        title={props.expensesDetails[2].title} 
+        amount={props.expensesDetails[2].amount} 
+        date={props.expensesDetails[2].date}/>     
+        <ExpenseItem 
+        title={props.expensesDetails[3].title} 
+        amount={props.expensesDetails[3].amount} 
+        date={props.expensesDetails[3].date}/>
+      </Card>
+    );
+};
+
+export default Expenses;
